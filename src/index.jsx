73ae06d3ad63b3dca0270f24e67bcc4c4bc35e6d
Dan Cork <dancork@gmail.com>
2016-05-06 14:52:10 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux'
 import App from './components/app'
 import reducers from './reducers'
 
-// COMPONENTS
 render(
   <Provider store={createStore(reducers)}>
     <App />
