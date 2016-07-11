436f69938308157e3360e6789bfe9e6259fd4a56
dancork <daniel.cork@holidayextras.com>
2016-07-11 22:18:46 +0000

    Node LTS and add thunk

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import App from './components/app'
-import reducers from './reducers'
+import rootReducer from './reducers'
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+)
 
 render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('app')
